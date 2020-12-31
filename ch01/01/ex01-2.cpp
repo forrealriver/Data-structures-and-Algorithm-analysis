a18487ec76f13d6568a0b8066739727bfa7883d2
forrealriver <294673951@qq.com>
2020-12-31 16:25:52 +0800

    ex02 ex03

---
+++ ch01/01/ex01-2.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<ctime>
+#include<fstream>
+#include<random>
+using namespace std;
+
+int arr[100010];
+
+void BubleSort(int arr[], int length)
+{
+	for (int i = length - 1; i != 0; --i) {       //sort in decreasing order
+		for (int j = 0; j != i; ++j) {
+			if (arr[j] < arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	ifstream in("test.txt");
+	ofstream out("test.txt");
+	uniform_int_distribution<unsigned> u(0, 200000);
+	default_random_engine e;
+	for (size_t i = 0; i < 100000; ++i) {
+		out << u(e) << " ";               //write random unsigned integer into the file
+	} 
+	int N, num; 
+	cin >> N;         //determine the number of elements to test
+	int k = N / 2; 
+	clock_t t;      //timing
+	t = clock(); 
+	for (int i = 0; i != k; ++i) {
+		in >> num;
+		arr[i] = num;
+	}
+	BubleSort(arr, k);
+	int temp = k;
+	while (temp--) {
+		in >> num;
+		if (num <= arr[k - 1])	continue;
+		else {
+			for (int i = k - 2; i >= 0; --i) {
+				if (arr[i] >= num) {
+					for(int j = k - 1;j != i + 1;--j) {
+						arr[j] = arr[j - 1];
+					}
+					arr[i + 1] = num;
+					break;
+				}
+			}
+		}
+	}
+	cout << arr[k - 1] << endl;
+	t = clock() - t;
+	cout << "It took " << static_cast<float>(t) / CLOCKS_PER_SEC << " seconds" << endl;    //output the running time 
+	return 0;
+}
---
+++ ch01/03/ex03.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void PrintDigit(size_t N)
+{
+	cout << N;
+}
+
+void PrintOut(size_t N)
+{
+	if (N >= 10)
+		PrintOut(N / 10);
+	PrintDigit(N % 10);
+}
+
+void PrintOut(double N, size_t precision)  //define precision to avoid round-off error
+{
+	if (N < 0) {
+		cout << '-';
+		N = -N;
+	}
+	int integer_part = static_cast<int>(N);   
+	PrintOut(integer_part);   
+	cout << '.';
+	double float_part = N - integer_part;
+	int dou_to_int= static_cast<int>(float_part * pow(10, precision));
+	PrintOut(dou_to_int);
+}
+
+int main()
+{
+	PrintOut(995);
+	cout << endl;
+	PrintOut(-88.888, 3);
+	cout << endl;
+	return 0;
+}
