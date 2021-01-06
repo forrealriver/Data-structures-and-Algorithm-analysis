94b0e642dea3c5472a5978082c7b2e709b603223
forrealriver <294673951@qq.com>
2021-01-06 18:43:57 +0800

    分治

--- ch02/DivideAndConquer.cpp
+++ ch02/DivideAndConquer.cpp
@@ -1,17 +1,74 @@
 #include <iostream>
-
+#include <ctime>
+#include <fstream>
+#include <random>
 using namespace std;
 
+int Max3(int a, int b,int c){
+    int max=0;
+    if(a>=b){
+        if(a>=c)
+            return a;
+        else
+            return c;
+        
+    }
+    else {
+        if(b >= c)
+            return b;
+        else return c;
+    }
+}
+
+int MaxSubSum(int a[], int left, int right){
+    int MaxLeftSum,MaxRightSum;
+    int MaxLeftBorderSum,MaxRightBorderSum;
+    int LeftBorderSum,RightBorderSum;
+    int Center;
+
+    if(left == right)
+        if(a[left] > 0)
+            return a[left];
+        else 
+            return 0;
+    Center = (left + right)/2;
+    MaxLeftSum = MaxSubSum(a, left,Center);
+    MaxRightSum = MaxSubSum(a, Center + 1,right);
+    MaxLeftBorderSum = 0;
+    LeftBorderSum = 0;
+    for(int i = Center;i >= left; i--){
+        LeftBorderSum += a[i];
+        if(LeftBorderSum > MaxLeftBorderSum)
+            MaxLeftBorderSum = LeftBorderSum;
+    }
+
+    MaxRightBorderSum = 0;
+    RightBorderSum = 0;
+    for(int i = Center + 1; i <= right; i++){
+        RightBorderSum += a[i];
+        if(RightBorderSum > MaxRightBorderSum)
+            MaxRightBorderSum = RightBorderSum;
+    }
+
+    return Max3(MaxLeftSum,MaxRightSum,MaxLeftBorderSum + MaxRightBorderSum);
+}
+
+
+int MaxSubsequenceSum(int a[], int n)
+{
+    return MaxSubSum(a,0,n-1);
+}
 int main()
 {
     int a[10000];
-    int N;
-    cin >> N;
-    for (i = 0; i != N; i++)
+    int n;
+    int MaxSum = 0;
+    cin >> n;
+    for (int i = 0; i != n; i++)
     {
         cin >> a[i];
     }
-    int MaxSum = MaxSubsequenceSum(a, N);
+    MaxSum = MaxSubsequenceSum(a, n);
 
     cout << MaxSum << endl;
     return 0;
